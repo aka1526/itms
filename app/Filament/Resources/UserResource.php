a34fda813f3bc71314cc786e46cdd04820d2cc5b
Akachai <akachai1526@hotmail.com>
2022-07-05 11:50:03 +0700

    Add the remaining file

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\BelongsToManyMultiSelect;
+use Illuminate\Support\Facades\Hash;
+
+use Livewire\Component;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\DateTimePicker::make('email_verified_at'),
+                Forms\Components\TextInput::make('password')
+                    ->password()
+                    ->nullable()
+                    ->maxLength(255)
+                    ->visibleOn(Pages\CreateUser::class)
+                    ->hiddenOn(Pages\EditUser::class)
+                    ->dehydrateStateUsing(fn ($state) => Hash::make($state))
+                    ->dehydrated(fn ($state) => filled($state))
+                    ->required( Pages\CreateUser::class),
+
+                    BelongsToManyMultiSelect::make('roles')->relationship('roles', 'name'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('email')->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('email_verified_at')
+                    ->dateTime()->sortable(),
+                Tables\Columns\TextColumn::make('created_at')->sortable()
+                    ->dateTime()->sortable(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
--- resources/views/theme/main_header.blade.php
+++ resources/views/theme/main_header.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>@yield('title','แจ้งซ่อมคอมพิวเตอร์/อุปกรณ์ Online')</title>
+    <title>@yield('title','ITMS::แจ้งซ่อมคอมพิวเตอร์/อุปกรณ์ Online')</title>
 
     <!-- Bootstrap -->
     <link href="/asset/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
