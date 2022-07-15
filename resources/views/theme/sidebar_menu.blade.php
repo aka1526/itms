024cfb8ad49c84a0145daf0df216b3fe1b1097b8
Akachai <akachai1526@hotmail.com>
2022-07-15 14:52:40 +0700

    Add the remaining file

--- resources/views/theme/sidebar_menu.blade.php
+++ resources/views/theme/sidebar_menu.blade.php
@@ -1,6 +1,10 @@
  <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
 
     <div class="menu_section">
+        <ul class="nav side-menu">
+            <li><a  href="/dashboards"><i class="fa fa-bar-chart"></i>ข้อมูลคอมพิวเตอร์ </a>
+            </li>
+        </ul>
       <h3>ตั้งค่าระบบพื้นฐาน</h3>
       <ul class="nav side-menu">
         <li><a><i class="fa fa-desktop"></i>ข้อมูลคอมพิวเตอร์<span class="fa fa-chevron-down"></span></a>
