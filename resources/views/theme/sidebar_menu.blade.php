3cf895cd0e1b8be4e40d4cdf4391856a2db85e5a
Akachai <akachai1526@hotmail.com>
2022-09-03 09:51:52 +0700

    Add the remaining file

--- resources/views/theme/sidebar_menu.blade.php
+++ resources/views/theme/sidebar_menu.blade.php
@@ -6,22 +6,25 @@
             </li>
         </ul>
         @if(isset(Auth::user()->name))
-        <h3>ข้อมูลซ่อมบำรุง</h3>
+        
         <ul class="nav side-menu">
             <li><a><i class="fa fa-calendar"></i>ข้อมูลซ่อมบำรุงคอมพิวเตอร์<span class="fa fa-chevron-down"></span></a>
               <ul class="nav child_menu">
+
+                <li><a href="/repairs">ระบบแจ้งซ่อม</a></li>
                 <li><a href="/pmplans">แผนซ่อมบำรุง</a></li>
+                <li><a href="/fixasset">ข้อมูลคอมพิวเตอร์</a></li>
                 {{-- <li><a href="/fixasset">ปฎิทินซ่อมบำรุง</a></li> --}}
 
               </ul>
             </li>
             </ul>
-      <h3>ตั้งค่าระบบพื้นฐาน</h3>
+     
       <ul class="nav side-menu">
-        <li><a><i class="fa fa-desktop"></i>ข้อมูลคอมพิวเตอร์<span class="fa fa-chevron-down"></span></a>
+        <li><a><i class="fa fa-desktop"></i>ค่าระบบพื้นฐาน<span class="fa fa-chevron-down"></span></a>
           <ul class="nav child_menu">
-            <li><a href="/repairs">รายการแจ้งซ่อม</a></li>
-            <li><a href="/fixasset">รายการคอมพิวเตอร์</a></li>
+
+          
             <li><a href="/problems">ข้อมูลปัญหา</a></li>
             <li><a href="/periods">ข้อมูลรอบเวลา</a></li>
             <li><a href="/checklists">ข้อมูล Check List </a></li>
