df2ef5983df5f2622a314b52c7f7d48a3b85f5e2
Akachai <akachai1526@hotmail.com>
2022-09-17 16:32:56 +0700

    Add the remaining file

--- resources/views/theme/main.blade.php
+++ resources/views/theme/main.blade.php
@@ -35,7 +35,7 @@
 			<div class="col-md-3 left_col">
 				<div class="left_col scroll-view">
 					<div class="navbar nav_title" style="border: 0;">
-						<a href="/" class="site_title"><i class="fa fa-paw"></i> <span>I</span>TMS</a>
+						<a href="/" class="site_title"><i class="fa fa-user-md"></i> <span>I</span>TMS</a>
 					</div>
 
 					<div class="clearfix"></div>
